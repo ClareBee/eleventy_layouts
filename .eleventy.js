ef0b87d802b87a4ce635ea78f599195452c84a17
Clare <chblackburne@gmail.com>
2019-08-22 18:26:28 +0100

    first njks template

--- .eleventy.js
+++ .eleventy.js
@@ -4,10 +4,12 @@ module.exports = function(eleventyConfig){
 
   return {
     passthroughFileCopy: true,
+    markdownTemplateEngine: 'njk',
+    templateFormats: ['html', 'njk', 'md'],
     dir: {
       input: "src",
       output: "_site",
-      include: "includes"
+      includes: "includes"
     }
   }
 }
